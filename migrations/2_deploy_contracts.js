afa05e682d597d8d210a846cfc6fe4b3d746932f
Vidhan Shah <shahvidhan123@gmail.com>
2021-07-27 19:02:36 +0530

    Migration Changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,19 @@
 const DStorage = artifacts.require("DStorage");
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(DStorage);
+module.exports = async function(deployer) {
+  // Deploy Token
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+  // Deploy EthSwap
+  await deployer.deploy(EthSwap, token.address);
+  const ethSwap = await EthSwap.deployed()
+
+  // Deploy DStorage
+  await deployer.deploy(DStorage);
+
+  // Transfer all tokens to EthSwap (1 million)
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
 };
