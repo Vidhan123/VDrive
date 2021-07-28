0eafee7154faf021abb87ba014660322b8d90d20
Vidhan Shah <shahvidhan123@gmail.com>
2021-07-29 02:28:04 +0530

    Additional Storage UI

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useStyles } from './styles';
 import clsx from 'clsx';
@@ -6,15 +6,129 @@ import { Drawer, List, Typography, Divider, IconButton, ListItem, ListItemIcon, 
 import { ChevronLeft, FolderShared, QueryBuilder, StarBorder, DeleteOutline, Storage, CloudQueue, LibraryBooks, DataUsage } from '@material-ui/icons';
 import Identicon from 'identicon.js';
 import LinearWithValueLabel from './LinearProgressWithLabel';
+import Swal from 'sweetalert2';
 import './App.css';
+import './Plans/Plans.css';
 
 import { myColor } from './helpers';
 
 function Sidebar(props) {
   const { open, handleDrawerClose, account, section, setSection, sizeUsed, totalSize } = props;
 
+  const [vidBal, setVidBal] = useState(0);
+
   let history = useHistory();
   const classes = useStyles();
+
+  const handleBuyStorage = (a) => {
+    console.log(a);
+  };
+
+  const handleBuyStorageModal = () => {
+    Swal.fire({
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      title: 'Additional Storage',
+      didOpen: () => {
+        document.getElementById("b1").addEventListener("click", function() {
+          handleBuyStorage(5);
+        });
+        document.getElementById("b2").addEventListener("click", function() {
+          handleBuyStorage(10);
+        });
+        document.getElementById("b3").addEventListener("click", function() {
+          handleBuyStorage(15);
+        });
+      },
+      html: 
+      `
+      <h3 style="text-align: left">Available Balance: ${vidBal} VID Tokens</h3> 
+      <br />
+      <br />
+      <div class="wrap">
+      <div class="con-items">
+              <div class="item item1" >
+                  <header>
+                      <h3>Basic</h3>
+                      <p>
+                          <b>
+                            50 VID Tokens
+                          </b>
+                      </p>
+                  </header>
+                  <ul>
+                      <li>
+                          <b>
+                            5 GB
+                          </b>
+                            Additional Storage
+                      </li>
+                  </ul>
+                  <button id="b1">
+                      Choose Plan 
+                  </button>
+              </div>
+              <div class="item color item2">
+                  <span class="badge">
+                      Popular
+                  </span>
+                  <header>
+                      <h3>Special</h3>
+                      <p>
+                          <b>
+                            75 VID Tokens
+                          </b>
+                      </p>
+                  </header>
+                  <ul>
+                      <li>
+                          <b>
+                              10 GB
+                          </b>
+                            Additional Storage
+                      </li>
+                  </ul>
+                  <button class="border" id="b2">
+                      Choose Plan 
+                  </button>
+              </div>
+              <div class="item item3">
+                  <header>
+                      <h3>Super Saver</h3>
+                      <p>
+                          <b>
+                              100 VID Tokens
+                          </b>
+                      </p>
+                  </header>
+                  <ul>
+                      <li>
+                          <b>
+                              15 GB
+                          </b>
+                            Additional Storage
+                      </li>
+                  </ul>
+                  <button id="b3">
+                      Choose Plan 
+                  </button>
+              </div>
+          </div>
+      </div>
+      <br />
+      <br />
+      `
+      ,
+      width: 1050,
+      showConfirmButton: false,
+      // confirmButtonText: 'Okay',
+      // icon: 'info',
+      backdrop: false,
+      customClass: {
+        container: 'my-swal'
+      }
+    })
+  };
 
   const myItemsArray = [
     {
@@ -100,11 +214,11 @@ function Sidebar(props) {
         </ListItem>
       </List>
       <List>
-        <ListItem button style={{ backgroundColor: "rgba(63,81,181,0.5)", color: "#fff" }}>
+        <ListItem button style={{ backgroundColor: "rgba(63,81,181,0.5)", color: "#fff" }} onClick={() => handleBuyStorageModal()}>
           <ListItemIcon>
             <Storage fontSize="large" style={{ color: "#fff" }} />
           </ListItemIcon>
-          <ListItemText primary="Buy Extra Storage" secondary="5 GB for 100 VID Tokens" />
+          <ListItemText primary="Buy Extra Storage" />
         </ListItem>
       </List>
     </div>
